98a633ff0e89e8836438b75b57357c73c96dd768
Giuseppe Velocci <giuseppe.velocci@msc.com>
2022-03-24 10:26:46 +0100

    simplified code

--- AkkaNetPlayground/AkkaAzureServiceBusCleaner/Actors/StreamingDeferralActor.cs
+++ AkkaNetPlayground/AkkaAzureServiceBusCleaner/Actors/StreamingDeferralActor.cs
@@ -38,13 +38,10 @@ namespace AkkaAzureServiceBusCleaner.Actors
 
         private Task StreamSequenceNumbers(long[] sequenceNumbers)
         {
-            var actorSink = Sink.ActorRef<Result>(Self, Result.IterationComplete);
             return Source.From(sequenceNumbers)
                 .Log("source")
                 .SelectAsync(1, x => CompleteMessageOrNot(x))
                 .Log("select")
-                .AlsoTo(actorSink)
-                .Log("toActor")
                 .RunWith(Sink.Ignore<Result>(), Materializer);
         }
 
@@ -70,17 +67,6 @@ namespace AkkaAzureServiceBusCleaner.Actors
                 case long[] sequenceNumbers:
                     Logger.Info("Received streaming request message by {res}", message);
                     await StreamSequenceNumbers(sequenceNumbers);
-                    break;
-                // just logging
-                case Result.Interrupted:
-                    Logger.Warning("Received interrupted");
-                    break;
-                case Result.ProcessingComplete:
-                case Result.ProcessingStopped:
-                    Logger.Info("Completed with status {message}", message);
-                    break;
-                case Result.IterationComplete: // To be fixed handling of rerun in case of error
-                    Logger.Info("Completed iteration");
                     break;
                 default:
                     Logger.Info("unexpected message");
